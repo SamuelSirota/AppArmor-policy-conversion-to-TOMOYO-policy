c1b6946771661b5e6c1929a871ee5543547c4dcb
SamuelSirota <xsirotas@stuba.sk>
2025-04-03 08:24:52 +0200

    feat: refactor from last week

--- apparmor_lark.py
+++ apparmor_lark.py
@@ -1,18 +1,33 @@
-from lark import Lark
-
+from lark import Lark, Transformer, v_args
+import os
 # Load the grammar
 with open("apparmor.lark", "r") as f:
     grammar = f.read()
 
-parser = Lark(grammar, start="start", parser="lalr")
+class AccessTransformer(Transformer):
+    def exclude_w_a_combination(self, items):
+        
+        access_list = [item[0] for item in items]
+        
+        if 'w' in access_list and 'a' in access_list:
+            raise ValueError("Access sequence cannot contain both 'w' and 'a'.")
+        return access_list
 
+parser = Lark(grammar, start="start", parser="lalr", transformer=AccessTransformer())
+folder_path = "/home/samos/FEI/ING/year2/diplomovka/tests/passes/"
 # Example policy
-try:
-    with open("tests/passes/variable.sd", "r") as f:
-        policy = f.read()
-    tree = parser.parse(policy)
-    print(tree.pretty())
-except Exception as e:
-    print("An exception occurred")
-    print(e)
+
+for filename in os.listdir(folder_path):
+    print(filename)
+    file_path = os.path.join(folder_path, filename)
+
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as f:
+            policy = f.read()
+        try:
+            tree = parser.parse(policy)
+            print(tree.pretty())
+        except Exception as e:
+            print("An exception occurred")
+            print(e)
     
---
+++ poznamky.md
@@ -0,0 +1,22 @@
+# stretnutie 27.3.2025
+
+- zapisovat pri ktorych to nefunguje pripadne pisat roderikovi nech pozrie
+- dalej asi ta semantika teda spracovavanie, (napr. ze write a append nemozu byt spolu)
+- REPREZENTACIA nejake objekty abo co
+- v AA su rwx... a v tomoyo tie systemove volania mkdir atd.
+- napr read v AA musim pridat v tomoyo vsetky syscally ktore citaju
+- reprezentacia asi bude pripominat AA
+- cim vseobecnejsie tym lepsie
+- plain old data
+
+- pri tych includoch si treda dat pozor na cykly, ak nieco je napr uz nacitane tak aby som to nenacitaval znova...
+- tie aby si nejake verzie aby to neenfocovalo veci z inych verzie kde nieco nebolo
+- search and replace pri tych premennych niektore su ale len regexy
+
+- AARE niektore napr * su podporovane aj v tomoyo ine napr {a,b} nie teda bue to treba rozbalit na rozne
+
+- skusit daco spustit v tomoyo a pozriet co nie je pokryte
+- este AARE to je ten globbing tie custom regexy
+
+- v tej praci ked pisem po anglicky tak bude treba este nakonci mat nejake vlastne zhrnutie v slovencine a malo by to pokryvat ze 10% prace, resume
+- updatnut poslednu verziu latex sablonu z 7.2. updatnut
